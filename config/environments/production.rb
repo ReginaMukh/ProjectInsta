1cdc53dce6c5bd6f9c0e440f6f618d801a08c9cc
ReginaMukh <mrr.woman@gmail.com>
2023-10-08 22:29:30 +0300

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,8 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
-
+  
+  config.log_level = :warn
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { instaproject.osc-fr1.scalingo.io }
 end
